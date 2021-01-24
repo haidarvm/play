488d9f95b334aae85d8dbab3f5a0f4dfaa6a52b8
haidarvm <haidarvm@gmail.com>
2021-01-24 23:26:39 +0700

    rm test

--- cuaca.php
+++ cuaca.php
@@ -3,7 +3,7 @@ $message = "/cuaca Pontianak";
 if (strpos($message, '/cuaca') === 0) {
     $location = substr($message, 7);
     // echo $location;
-    $apiopenweahtermap = '14329a473635baf8ea2c6e29107d1bbe';
+    $apiopenweahtermap = '';
     $openweahtermap = 'http://api.openweathermap.org/data/2.5/weather?q=' . $location . '&appid=' . $apiopenweahtermap.'&lang=id&units=metric';
     // echo $openweahtermap;
     $weather = json_decode(file_get_contents($openweahtermap), true);
--- match.php
+++ match.php
@@ -1,3 +1,21 @@
 <?php
+$text = 'L\' utente _{nickname} ti ha invitato a giocare';
+preg_match('#\_{(.*?)\}#', $text, $matches);
+echo json_encode($matches);
 
-$text_vars = preg_match('#\_{(.*?)\}#', $text, $matches);
+$a = '/test Bandung?';
+
+echo matchCom($a);
+echo matching("_{nickname}");
+function matchCom($command) {
+    if (strpos($command, '/cuaca') !== false) {
+        return  'cuaca adalah';
+    } elseif (strpos($command, '/test') !== false) {
+        return  'test adalah';
+    }
+}
+
+function matching($command) {
+    preg_match('#\_{(.*?)\}#', $command, $matches);
+    return $matches[0];
+}
